9cd6e45fb6de8ad33b518f56f4705d2c960b1ae9
PHYSCO_SPY <89356302+PHYSCOSPY@users.noreply.github.com>
2022-06-13 01:02:57 +0530

    Add files via upload

---
+++ Admin.py
@@ -0,0 +1,126 @@
+print("\033[2;33;40m\n𝐖𝐄𝐋𝐋𝐂𝐎𝐌𝐄")
+print()
+print()
+
+
+import time
+from time import sleep
+
+
+a=20
+b=2
+while b<a:
+  print("\033[1;32;40m\nInstaling................!!")
+  sleep (0.5)
+  b += 1
+print()
+print()
+print()
+
+import time
+from time import sleep
+
+
+a=5
+b=1
+while b<a:
+     print("\033[3;31;40m\nPlease Wait...............!!")
+     sleep (0.3)
+     b += 1
+print()
+print( "\033[0;36;40m\nInstaling successfull..........!!" )
+print()
+print()
+print()
+print("\033[0;32;40m\n !......▆▆         ▆▆.....!")
+print("\033[0;32;40m\n !......▆▆         ▆▆.....!")
+print("\033[0;31;40m\n !......▆▆▆▆▆▆▆▆▆▆▆▆.....!")
+print("\033[0;32;40m\n !......▆▆         ▆▆.....!")
+print("\033[0;32;40m\n !......▆▆         ▆▆.....!")
+print()
+print()
+
+import os
+os.system("figlet Group Hijack|lolcat ")
+print()
+print()
+x=str(input("Enter The Hijack Group link :-"))
+print()
+print()
+
+import time
+from time import sleep
+x=100
+y=3
+while y<x:
+    print("\033[2;34;40m\n𝚂𝚎𝚊𝚛𝚌𝚑𝚒𝚗𝚐.........................................!!!!")
+    sleep (0.5)
+    y +=10
+print()
+print()
+print("\033[1;37;46m\n𝙲𝚘𝚖𝚙𝚕𝚒𝚝𝚎𝚍.....")
+print()
+x=int(input("\033[0;37;40m\n𝙴𝚗𝚝𝚎𝚛 𝙲𝚛𝚎𝚊𝚝𝚎𝚛 𝙰𝚍𝚖𝚒𝚗 𝙽𝚞𝚖𝚋𝚎𝚛 :-"))
+print()
+print(x)
+print()
+print("\033[0;32;40m\nρℓєαѕє ωαιт:::::::::!")
+print()
+
+print("\033[0;32;40m\nρℓєαѕє ωαιт:::::::::!")
+
+import os
+
+os.system("")
+import os
+import time
+os.system("figlet -f banner HIJACKER |lolcat -a -d 3")
+print()
+from time import sleep
+x= 300
+y= 0
+while x>y :
+        print ("\033[0;32;40m\nAttempt.... ", 1+y)
+        sleep (0.3)
+        y+=1
+        if y== 30:
+                print ()
+                print("\033[0;31;40m\nSome error occurred .please try again>")
+                print()
+                break
+print("Sorry.............")
+print("Please Try Again.............")
+print()
+print("⚠Warning⚠")
+print()
+num1 = int(input("\033[0;33;40mEnter Creater Admin Number:-"))
+print(num1)
+
+anju = input("\033[0;31;40mDO YOU WANT TO HIJACK (y/n ) : ")
+if anju == "y" or  anju == "Y" :
+	print("\033[0;32;40mok started hijacking.....")
+elif  anju == "n" or  anju == "N" :
+	print ("\033[0;36;40ok come again...")
+	exit()
+else :
+	print ("Invalid input ")
+	exit()
+print()
+print()
+
+x=100
+y=5
+while x>y :
+	print("\033[2;32;40mTring to HIJACK............!")
+	sleep(1)
+	y += 5
+	print()
+print("\033[0;33;40m\nඅනේ නිකම් පලයන් යන්න ආවා මෙතන හයිජැක් කරන්න..\n ආපරාදෙ කාලෙ හුත්තො😂")
+print()
+print("\033[0;36;40m\nඋබ හෙන පවු බන්😂")
+print()
+import os
+os.system("figlet PRANK|lolcat ")
+print()
+print("\033[0;36;40m\nඑහෙනම් කොල්ලො අපි ගියා.....✌\n Bye✌")
+print("මෙක පුරුදුවෙන්න හදපු එකක් 😁")
